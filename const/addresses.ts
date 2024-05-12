2f2c0f4a4d66f6f8fdbffb191c62c055bfe23e7d
Vueltarapidagame <154387028+Vueltarapidagame@users.noreply.github.com>
2024-05-11 22:55:24 -0400

    Update addresses.ts

--- const/addresses.ts
+++ const/addresses.ts
@@ -1,5 +1,5 @@
-export const TRANSFER_CONTRACT_ADDRESS = '0x866724aD496bE40211E68ef9f2D2B8Ea0d991A65';
-export const CALIM_TOKEN_CONTRACT_ADDRESS = '0x61AD50f46CaB1121d307b0c9c93859a6559947a7';
+export const TRANSFER_CONTRACT_ADDRESS = '0x654D0288AbBc102429DaF70C413042a62D4085a1';
+export const CALIM_TOKEN_CONTRACT_ADDRESS = '0xC63E272a2DBa2c5f7bb66d99389b3Dc31Ec515cD';
 
 export const HERO_IMAGE_URL = 'ipfs://QmUWVRuyFuVfo45estsJt2VaQUSEc4uwcPoeWFaeYxERua/seanwatase_isometric_crypto_transferring_from_one_wallet_to_ano_96084ff1-fe03-4dfd-8783-a3ecc85a24da.png';
 export const FEATURES_IMAGE_URL = 'ipfs://QmcTgfb2AXj1TcVj4i9jSgZc8A8LhxHYCXbu5Mngsx7iVT/Coins.png';
